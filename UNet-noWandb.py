a0f7d8369e2f2c37eea744cc1e8e1aa3d2c6889f
Tymoteusz <stymex@gmail.com>
2022-01-06 14:34:33 +0100

    added unet traing without wandb

---
+++ UNet-noWandb.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 22 13:25:39 2021
+
+@author: stymex
+"""
+import sys
+import glob
+import cv2
+import numpy as np
+import segmentation_models as sm
+from segmentation_models import losses
+from segmentation_models import metrics
+from segmentation_models.losses import bce_dice_loss
+from tensorflow.keras.models import load_model
+from sklearn.model_selection import train_test_split
+from LoadDataset import load_dataset
+import albumentations as A
+
+resume = sys.argv[-1] == "--resume"
+
+config = dict(
+    optimizer="Adam",
+    epoch=100,
+    batch_size=64,
+    backbone="resnet34",
+    architecture="Unet"
+    )
+
+training_images, training_images_masks = load_dataset()
+training_images_masks = np.expand_dims(training_images_masks, axis=3)
+
+BACKBONE = config['backbone']
+preprocess_input = sm.get_preprocessing(BACKBONE)
+
+x_train, x_val, y_train, y_val = train_test_split(training_images, training_images_masks, test_size=0.2, random_state=42)
+
+# augument data
+
+transform = A.Compose([
+    A.Transpose(always_apply=True, p=0.5),
+    A.HorizontalFlip(always_apply=True, p=0.8),
+    A.VerticalFlip(always_apply=True, p=0.8),
+    A.RandomRotate90(always_apply=True, p=0.8),
+    A.RandomBrightnessContrast(always_apply=True, p=0.8, brightness_limit=(-0.3, 0.3), contrast_limit=(-0.3, 0.3)),
+    A.Blur(always_apply=True, p=0.5, blur_limit=(3, 7)),
+    A.GaussNoise(always_apply=True, p=0.5, var_limit = (10.0,200.0)),
+])
+
+augumented_x = list()
+augumented_y = list()
+for img, mask in zip(x_train, y_train):
+    transformed = transform(image=img, mask=mask)
+    augumented_x.append(transformed['image'])
+    augumented_y.append(transformed['mask'])
+
+augumented_x = np.array(augumented_x, dtype=np.float32)
+augumented_y = np.array(augumented_y, dtype=np.float32)
+
+# transformed = transform(image=x_train, mask=y_train)
+# augumented_x = transformed['image']
+# augumented_y = transformed['mask']
+
+x_train = np.concatenate((x_train, augumented_x), axis=0)
+y_train = np.concatenate((y_train, augumented_y), axis=0)
+
+# preprocess input
+x_train = preprocess_input(x_train)
+x_val = preprocess_input(x_val)
+    
+# define model
+model = sm.Unet(BACKBONE, 
+                input_shape=(None, None, 3), 
+                classes=1, 
+                activation='sigmoid', 
+                weights=None, 
+                encoder_weights='imagenet', 
+                encoder_freeze=True, 
+                encoder_features='default', 
+                decoder_block_type='transpose', 
+                decoder_filters=(256, 128, 64, 32, 16), 
+                decoder_use_batchnorm=True)
+
+model.compile(
+    optimizer = config['optimizer'],
+    loss=bce_dice_loss,
+    metrics=[metrics.iou_score, metrics.f1_score, metrics.f2_score, metrics.precision, metrics.recall],
+)
+
+print(model.summary())
+
+
+history = model.fit(x=x_train, y=y_train,
+                    epochs=config['epoch'],
+                    batch_size=config['batch_size'],
+                    validation_data=(x_val, y_val),
+                    )
+
